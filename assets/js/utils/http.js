7fb17c5155ffdcc6f8218e1974a356e2b38ccb96
wrrs <67336084+williamsalas@users.noreply.github.com>
2025-09-13 13:13:41 -0700

    fix caching for loadJson (#51)

--- assets/js/utils/http.js
+++ assets/js/utils/http.js
@@ -1,5 +1,6 @@
-export async function loadJson(url) {
-  const res = await fetch(url, { cache: "no-store" });
+export async function loadJson(url, opts = {}) {
+  const { cache = "default", ...rest } = opts;
+  const res = await fetch(url, { cache, ...rest });
   if (!res.ok) throw new Error(`HTTP ${res.status}`);
   return res.json();
 }
