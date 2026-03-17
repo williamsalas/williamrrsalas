0a020ecde26343c9b55663917d278165228970f5
wrrs <67336084+williamsalas@users.noreply.github.com>
2026-03-16 20:26:42 -0700

    replace react-toggle with custom css (#85)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module "react-toggle" {
-  import { Component, InputHTMLAttributes } from "react";
-  interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
-    icons?: boolean | { checked: React.ReactNode; unchecked: React.ReactNode };
-  }
-  export default class Toggle extends Component<ToggleProps> {}
-}
-
-declare module "react-toggle/style.css";
